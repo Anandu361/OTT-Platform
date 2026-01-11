293aa06b91970b4ba215441f98ce39d7bb1d1553
Anandu <anandusujakumari389@gmail.com>
2026-01-12 00:09:36 +0530

    Frontend for both admin and user roles

---
+++ frontend/src/components/routes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LandingPage from '../pages/LandingPage.jsx'
+import Login from '../pages/Login.jsx'
+import Signup from '../pages/Signup.jsx'
+
+function routes() {
+  return (
+    <Routes>
+      <Route path='/' element={<LandingPage />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
+  )
+}
+
+export default routes
